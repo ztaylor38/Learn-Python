a0e89ab330b038bd1a3d60267fe53f50c267506b
ztaylor38 <134661851+ztaylor38@users.noreply.github.com>
2024-05-14 19:15:32 -0500

    update

---
+++ HelloWorld/Block Letters/initials.py
@@ -0,0 +1,9 @@
+# Zackery Taylor
+# I like to play guitar
+
+print("ZZZZZ    TTTTT")
+print("   Z       T")
+print("  Z        T")
+print(" Z         T")
+print("Z          T")
+print("ZZZZZ      T")
